01d6a9d115a9158be991b531b903f2079de605cf
josecervantes21 <jose.ocampo@tecmilenio.mx>
2023-10-31 17:31:35 -0600

    Local deployment

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ function closeBenieBot() {
   }
 }
 
+//This component help to the people for refresh the iframe
 function refresh() {
   var beniebotContainer = document.querySelector('.beniebot');
   var beniebotIframe = document.getElementById('beniebotIframe');
-  beniebotIframe.src = "chatbot/beniebot.html";
+  //reference for coustum benie bot 
+  beniebotIframe.src = "/beniebot.html";
   beniebotContainer.style.display = 'block';
 }
 
@@ -25,12 +27,28 @@ const openBot = document.getElementById("openNewWindow");
 const agreementCheckbox = document.getElementById("agreementCheckbox");
 const errorSpan = document.getElementById("js-chatbot-error");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const agreementCheckbox = document.getElementById("agreementCheckbox");
+  if (getCookie("agreement") === "true") {
+    agreementCheckbox.checked = true;
+  }
+  function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+});
+
 openBot.addEventListener("click", function () {
   if (agreementCheckbox.checked) {
     var beniebotContainer = document.querySelector('.beniebot');
     var beniebotIframe = document.getElementById('beniebotIframe');
     beniebotIframe.src = "beniebot.html";
     beniebotContainer.style.display = 'block';
+    var expirationDate = new Date();
+    expirationDate.setDate(expirationDate.getDate() + 1);
+    document.cookie = "username=Employee; path=/; expires=" + expirationDate.toUTCString();
+    document.cookie = "agreement=true; path=/; expires=" + expirationDate.toUTCString();
     closeDisclaimer();
   } else {
     errorSpan.textContent = "You must agree to the disclaimers to activate the Virtual Assistant.";
@@ -39,41 +57,41 @@ openBot.addEventListener("click", function () {
     });
   }
 });
-document.addEventListener("DOMContentLoaded", function () {
-  const openButton = document.querySelector(".chatbot_icon");
-  const botContent = document.querySelector(".disclaimer");
+function getCookie(name) {
+  const value = "; " + document.cookie;
+  const parts = value.split("; " + name + "=");
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
 
-  window.onload = function() {
-    if (getCookie("agreement") === "true") {
-      agreementCheckbox.checked = true;
-    }
-  };
-  
-  openButton.addEventListener("click", function () {
-    if (agreementCheckbox.checked) {
-      var beniebotContainer = document.querySelector('.beniebot');
-      var beniebotIframe = document.getElementById('beniebotIframe');
-      beniebotIframe.src = "https://topmexico.org/chatbot/beniebot.html";
-      document.cookie = "username=Employee; path=/";
-      document.cookie = "agreement=true;"; // Establece la cookie de acuerdo
-      beniebotContainer.style.display = 'block';
-      closeDisclaimer();
-    } else {
-      if (botContent.style.display === "none") {
-        botContent.style.display = "block";
+document.addEventListener("DOMContentLoaded", function () {
+  //Add your custom domain
+  const domain = "";
+  if (window.location.hostname.endsWith(domain)) {
+    const openButton = document.querySelector(".chatbot_icon");
+    const botContent = document.querySelector(".disclaimer");
+    const agreementCheckbox = document.getElementById("agreementCheckbox");
+
+    openButton.addEventListener("click", function () {
+      if (agreementCheckbox.checked) {
+        var beniebotContainer = document.querySelector('.beniebot');
+        var beniebotIframe = document.getElementById('beniebotIframe');
+        beniebotIframe.src = "" + domain + "beniebot.html";
+        beniebotContainer.style.display = 'block';
+        closeDisclaimer();
       } else {
-        botContent.style.display = "none";
+        if (getCookie("agreement") !== "true") {
+          if (botContent.style.display === "none") {
+            botContent.style.display = "block";
+          } else {
+            botContent.style.display = "none";
+          }
+        }
       }
-    }
-  });
-  
-  // Función para obtener el valor de la cookie por su nombre
-  function getCookie(name) {
-    const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-    if (parts.length === 2) return parts.pop().split(";").shift();
+    });
   }
 });
+
+
 
 
 
