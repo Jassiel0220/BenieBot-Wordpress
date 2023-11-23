f45ee931dbc0fbab0a9c76dba6f95ca9e4d1cf29
Jose Cervantes <jose.cervantes@innadvisory.com>
2023-11-23 17:13:24 -0600

    Session ID localstorage

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 function closeBenieBot() {
   var beniebotDiv = document.querySelector('.beniebot');
   beniebotDiv.style.display = 'none';
-  var iframe = document.getElementById("beniebotIframe");
-  if (iframe) {
-    iframe.src = 'about:blank';
-  }
+ 
 }
-
+ 
 //This component help to the people for refresh the iframe
 function refresh() {
   var beniebotContainer = document.querySelector('.beniebot');
   var beniebotIframe = document.getElementById('beniebotIframe');
-  //reference for coustum benie bot 
-  beniebotIframe.src = "/beniebot.html";
+  //reference for coustum benie bot
+  beniebotIframe.src = "beniebot.html";
   beniebotContainer.style.display = 'block';
 }
-
+ 
 function closeDisclaimer() {
   const disclaimer = document.querySelector(".disclaimer");
   if (disclaimer) {
     disclaimer.style.display = "none";
   }
 }
-
+ 
 const openBot = document.getElementById("openNewWindow");
 const agreementCheckbox = document.getElementById("agreementCheckbox");
 const errorSpan = document.getElementById("js-chatbot-error");
-
+ 
 document.addEventListener("DOMContentLoaded", function () {
   const agreementCheckbox = document.getElementById("agreementCheckbox");
   if (getCookie("agreement") === "true") {
@@ -38,17 +35,17 @@ document.addEventListener("DOMContentLoaded", function () {
     if (parts.length === 2) return parts.pop().split(";").shift();
   }
 });
-
+ 
 var isBotActivated = localStorage.getItem("botActivated");
-
-
+ 
+ 
 openBot.addEventListener("click", function () {
   if (agreementCheckbox.checked) {
-    
+   
     localStorage.setItem("botActivated", true);
-
-
-    
+ 
+ 
+   
     var beniebotContainer = document.querySelector('.beniebot');
     var beniebotIframe = document.getElementById('beniebotIframe');
     beniebotIframe.src = "beniebot.html";
@@ -58,9 +55,9 @@ openBot.addEventListener("click", function () {
     document.cookie = "username=Employee; path=/; expires=" + expirationDate.toUTCString();
     document.cookie = "agreement=true; path=/; expires=" + expirationDate.toUTCString();
     closeDisclaimer();
-
+ 
   }
-  
+ 
   else {
     errorSpan.textContent = "You must agree to the disclaimers to activate the Virtual Assistant.";
     agreementCheckbox.addEventListener("change", function () {
@@ -73,39 +70,46 @@ function getCookie(name) {
   const parts = value.split("; " + name + "=");
   if (parts.length === 2) return parts.pop().split(";").shift();
 }
-
+ 
 document.addEventListener("DOMContentLoaded", function () {
-  //Add your custom domain
-  const domain = "";
-  if (window.location.hostname.endsWith(domain)) {
-    const openButton = document.querySelector(".chatbot_icon");
-    const botContent = document.querySelector(".disclaimer");
-    const agreementCheckbox = document.getElementById("agreementCheckbox");
-
-    openButton.addEventListener("click", function () {
-      if (agreementCheckbox.checked) {
-        var beniebotContainer = document.querySelector('.beniebot');
-        var beniebotIframe = document.getElementById('beniebotIframe');
-        beniebotIframe.src = "" + domain + "beniebot.html";
-        beniebotContainer.style.display = 'block';
-        closeDisclaimer();
-      } else {
-        if (getCookie("agreement") !== "true") {
-          if (botContent.style.display === "none") {
-            botContent.style.display = "block";
-          } else {
-            botContent.style.display = "none";
-          }
+  const domain = "http://127.0.0.1:5500/";
+ 
+  function setCookie(name, value) {
+    document.cookie = `${name}=${value}; path=/`;
+  }
+ 
+  function getCookie(name) {
+    const value = "; " + document.cookie;
+    const parts = value.split("; " + name + "=");
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
+ 
+  const openButton = document.querySelector(".chatbot_icon");
+  const botContent = document.querySelector(".disclaimer");
+  const agreementCheckbox = document.getElementById("agreementCheckbox");
+ 
+  const beniebotContainer = document.querySelector('.beniebot');
+  const beniebotIframe = document.getElementById('beniebotIframe');
+ 
+  openButton.addEventListener("click", function () {
+    if (agreementCheckbox.checked) {
+      if (!beniebotIframe.src || beniebotIframe.src === "about:blank") {
+        beniebotIframe.src = domain + "beniebot.html";
+      }
+ 
+      beniebotContainer.style.display = 'block';
+ 
+      setCookie('conversationInProgress', 'true');
+ 
+      closeDisclaimer();
+    } else {
+      if (getCookie("agreement") !== "true") {
+        if (botContent.style.display === "none") {
+          botContent.style.display = "block";
+        } else {
+          botContent.style.display = "none";
         }
       }
-    });
-  }
+    }
+  });
 });
-
-
-
-
-
-
-
-
